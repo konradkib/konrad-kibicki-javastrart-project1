cb60cc8acd09079449ddf73efdee714d5ce73ded
KonradKib <konrad.kibicki@gmail.com>
2021-08-25 18:12:51 +0200

    Library v0.1

---
+++ src/Library.java
@@ -0,0 +1,24 @@
+public class Library {
+
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.1";
+
+        String title = "W pustyniu i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releaseDate = 2010;
+        int pades = 296;
+        String publisher = "Greg";
+        String isbn = "9846542165";
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pades);
+        System.out.println(publisher);
+        System.out.println(isbn);
+
+
+    }
+}
