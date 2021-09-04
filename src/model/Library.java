1f3b413d85044f398c3910af364d3c5e69780a78
KonradKib <konrad.kibicki@gmail.com>
2021-09-04 14:39:42 +0200

    new opitons added

--- src/model/Library.java
+++ src/model/Library.java
@@ -4,33 +4,49 @@ import java.util.Scanner;
 
 public class Library {
 
-    private final int  maxbooks = 1000;
-    Book [] books = new Book[maxbooks];
-    Scanner sc = new Scanner(System.in);
+    private static final int MAX_BOOKS = 1000;
+    private static final int MAX_MAGAZINES = 1000;
+    private Book[] books = new Book[MAX_BOOKS];
+    private Magazine[] magazines = new Magazine[MAX_MAGAZINES];
     private int booksNumber;
+    private int magazinesNumber;
 
-
-
-
-    public void addBook(Book book){
-        if(booksNumber < maxbooks){
+    public void addBook(Book book) {
+        if (booksNumber < MAX_BOOKS) {
             books[booksNumber] = book;
             booksNumber++;
-        }else{
-            System.out.println("Maksymalna liczba książek została osiągnięta");
+        } else {
+            System.out.println("Maxymalna liczba książek została osiągnięta");
         }
 
     }
 
-    public void printBooks(){
-        if(booksNumber == 0) {
-            System.out.println("Brak ksiażek w bibliotece");
+    public void printBooks() {
+        if (booksNumber == 0) {
+            System.out.println("Brak książek w bibliotece");
         }
-
         for (int i = 0; i < booksNumber; i++) {
             books[i].printInfo();
         }
     }
 
+    public void addMagazine(Magazine magazine) {
+        if(magazinesNumber < MAX_MAGAZINES) {
+            magazines[magazinesNumber] = magazine;
+            magazinesNumber++;
+        } else {
+            System.out.println("Maxymalna liczba magazynów została osiągnięta");
+        }
 
+    }
+
+    public void printMagazines() {
+        if (magazinesNumber == 0) {
+            System.out.println("Brak magazynów w bibliotece");
+        }
+        for (int i = 0; i < magazinesNumber; i++) {
+            magazines[i].printInfo();
+        }
+
+    }
 }
